4927e696de0c9e57671c5d4d5b58cf8037899a55
Jean Souza <jeancfsouza@gmail.com>
2015-12-20 21:06:14 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    load = require('express-load'),
+    path = require('path'),
+    app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
+
+module.exports = app;
+
+app.listen(3000, function() {
+  console.log("Ntalk no ar!");
+});
